792ad8283726b99c796753058d13fc34660a05ce
Louis Gandelin <lgandelin@web-access.fr>
2015-06-25 15:38:34 +0200

    Refactoring

--- src/Webaccess/WCMSLaravel/Helpers/Module.php
+++ src/Webaccess/WCMSLaravel/Helpers/Module.php
@@ -2,17 +2,15 @@
 
 namespace Webaccess\WCMSLaravel\Helpers;
 
-use Webaccess\WCMSLaravel\Facades\Shortcut;
-
 class Module
 {
-    public static function load($moduleName)
+    public static function load($module)
     {
-        $themeFolder = base_path() . '/themes/' . Shortcut::get_theme();
-        \Lang::addNamespace('w-cms-laravel-' . $moduleName, $themeFolder . '/lang/modules/' . $moduleName);
-        \Lang::addNamespace('w-cms-laravel-' . $moduleName . '-back', base_path('resources/lang/vendor/' . $moduleName . '/'));
+        $themeFolder = base_path() . '/themes/' . Theme::get();
+        \Lang::addNamespace('w-cms-laravel-' . $module, $themeFolder . '/lang/modules/' . $module);
+        \Lang::addNamespace('w-cms-laravel-' . $module . '-back', base_path('resources/lang/vendor/' . $module . '/'));
 
-        \View::addNamespace('w-cms-laravel-' . $moduleName, $themeFolder . '/views/modules/' . $moduleName);
-        \View::addNamespace('w-cms-laravel-' . $moduleName . '-back', base_path('resources/views/vendor/' . $moduleName . '/'));
+        \View::addNamespace('w-cms-laravel-' . $module, $themeFolder . '/views/modules/' . $module);
+        \View::addNamespace('w-cms-laravel-' . $module . '-back', base_path('resources/views/vendor/' . $module . '/'));
     }
 }
